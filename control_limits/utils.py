b5233b98f53cee8155908a6544d2a7e73666f882
Domenico Tomaselli <domenico.tomaselli13@gmail.com>
2021-06-18 09:32:13 +0200

    Update utils.py

--- control_limits/utils.py
+++ control_limits/utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+from enum import Enum
 
 
 def assert_input(array, labels):
@@ -39,3 +40,70 @@ def assert_params(array, precision_limits, len_limits, shape_limits):
         raise AssertionError
     elif shape_limits < 0 or shape_limits > 1:
         raise AssertionError
+        
+        
+        
+ class RouteLimits(Enum):
+   """Signal which direction to construct the control limits"""
+
+   LEFT = 0
+   RIGHT = 1
+
+
+class ShiftSplit(Enum):
+    """Signal selecting low or high split value."""
+
+    LOW = 0
+    HIGH = 1
+    
+    
+def scale_precision_input(precision_limits, input_min=0.5, input_max=1.0, start_interval=2.0, end_interval=-2.0):
+    """Derive beta for f-beta score from user input
+    :param precision_limits: user input precision
+    :type precision_limits: float
+    :param input_min: minimum possible precision
+    :type input_min: float
+    :param input_max: maximum possible precision
+    :type input_max: float
+    :param start_interval: start of transformed interval
+    :type start_interval: float
+    :param end_interval: end of transformed interval
+    :type end_interval: float
+    :return: f-beta score
+    :rtype: float
+    """
+
+    scaling_factor = (end_interval - start_interval)
+    return scaling_factor * ((precision_limits - input_min) / (input_max - input_min)) + start_interval
+
+
+def euc_distance(array_ref, array):
+    """Return the euclidean distance
+
+    :param array_ref: reference data
+    :type array_ref: numpy array
+    :param array: input data
+    :type array: numpy array
+    :return: euclidean distance
+    :rtype: float
+    """
+
+    euc_distance = float(0)
+    for i, j in zip(array_ref, array):
+        euc_distance += np.sqrt((i - j) ** 2)
+    return euc_distance
+
+
+def linear_regression(time_steps, dec_boundary):
+    """Linear regression on the split points
+
+    :param time_steps: decision boundary time-steps
+    :type time_steps: numpy array
+    :param dec_boundary: decision boundary
+    :type dec_boundary: numpy array
+    :return: linear regression of the decision boundary
+    :rtype: numpy array
+    """
+
+    linear_fit = np.poly1d(np.polyfit(time_steps, dec_boundary, 1))
+    return linear_fit(time_steps)
